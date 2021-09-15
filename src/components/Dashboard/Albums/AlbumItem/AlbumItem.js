5a60e7ff9983d501f8957fde52176da6f59f15b7
Sanyi Toth <sandor.toth@hotmail.com>
2021-09-15 18:28:04 +0200

    modal works

--- src/components/Dashboard/Albums/AlbumItem/AlbumItem.js
+++ src/components/Dashboard/Albums/AlbumItem/AlbumItem.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import {Card, Heading, Media} from "react-bulma-components";
+import React, {useState} from 'react';
+import {Box, Button, Card, Content, Heading, Media, Modal} from "react-bulma-components";
 import './AlbumItem.css'
+import AlbumModal from "../AlbumModal/AlbumModal";
 
 function AlbumItem({album}) {
 
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
     return (
-        <Card className="AlbumItem" style={{width: 300, margin: 'auto'}}>
+        <Card className="AlbumItem" key={album.id} style={{width: 300, margin: 'auto'}}>
             <Card.Content>
                 <Media>
                     <Media.Item>
-                        <Heading size={4}>Album Id {album.id}</Heading>
+                        <Heading onClick={() => setIsModalOpen(true)} size={4}>Album Id {album.id}</Heading>
                     </Media.Item>
                 </Media>
             </Card.Content>
+            <AlbumModal album={album} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
         </Card>
     );
 }
--- src/components/Dashboard/Albums/AlbumModal/AlbumModal.js
+++ src/components/Dashboard/Albums/AlbumModal/AlbumModal.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Box, Button, Content, Media, Modal} from "react-bulma-components";
 
-function AlbumModal(props) {
+
+function AlbumModal({album,isModalOpen, setIsModalOpen}) {
+
+
+
     return (
-        <div></div>
+        <div>
+            <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <Modal.Content>
+                    <Box>
+                        <Media src='https://bulma.io/images/placeholders/128x128.png'>
+                            <Content>
+                                <p>
+                                    <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                                    <br/>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu
+                                    pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis
+                                    feugiat facilisis.
+                                </p>
+                            </Content>
+
+                        </Media>
+                    </Box>
+                </Modal.Content>
+            </Modal>
+
+
+        </div>
     );
 }
 
