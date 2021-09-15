e4f133e6b05c6af198194793b8e23a17f21a183f
Sanyi Toth <sandor.toth@hotmail.com>
2021-09-15 18:40:19 +0200

    refactor

--- src/components/Dashboard/Header/UserProfileCard/UserProfileCard.js
+++ src/components/Dashboard/Header/UserProfileCard/UserProfileCard.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {Card,  Heading,  Media} from "react-bulma-components";
+import {Heading, Media} from "react-bulma-components";
 
 function UserProfileCard({user}) {
     return (
-        <Card style={{width: 300, margin: 'auto'}}>
-            <Card.Content>
-                <Media alignItems="center">
-                    <Media.Item>
-                        <Heading size={4}>{user.name}</Heading>
-                        <Heading subtitle size={6}>
-                            Tel.: {user.phone} <br/>
-                            Email: {user.email}<br/>
-                            {`Address: ${user.address.zipcode}, ${user.address.city}, ${user.address.street} ${user.address.suite}`}
-                        </Heading>
-                    </Media.Item>
-                </Media>
-            </Card.Content>
-        </Card>
+        <Media style={{width: 300, padding: 15, margin: 'auto'}}>
+            <Media.Item>
+                <Heading size={4}>{user.name}</Heading>
+                <Heading subtitle size={6}>
+                    <strong>Tel.:</strong> {user.phone} <br/>
+                    <strong>Email:</strong> {user.email}<br/>
+                    <strong>Address:</strong> {user.address.zipcode}, {user.address.city},
+                    {user.address.street} {user.address.suite}
+                </Heading>
+            </Media.Item>
+        </Media>
+
     );
 }
 
